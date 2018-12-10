e44f94246b53f7de6982a09a4ea0fe987a3edbea
SrtaCamelo <raissa_camelo@hotmail.com>
2018-12-10 21:31:29 -0200

    Fabioooooooooooooooooh

--- ProjetoPy/memoryGerencia.py
+++ ProjetoPy/memoryGerencia.py
@@ -13,17 +13,60 @@ from random import shuffle
 Abre o Arquivo txt onde estão os processos (Equações)
 Retorna uma Lista de Processos aleatórizada
 """
+
+memory = []
+index = []
+size = 0
 def openfile(path):
     lines = [line.rstrip('\n') for line in open(path)]
-    shuffle(lines)
     return lines
-def Initialize_memory(n):
-    memory = []
+def calculateCost(proc):
+    #print(proc)
+    result = ''.join([i for i in proc if not i.isdigit()])
+    #print(result)
+    cost = 0
+    for i in result:
+        if i == '+' or i =='-':
+            cost += 1
+        elif i == '*' or i =='/':
+            cost += 3
+        elif i == '^':
+            cost += 5
+    return cost
+def inicialize_memory():
+    for n in range(50):
+        print(n)
+        memory.append(0)
+        index.append(0)
+
+def incert_into_memory(id, cost):
+    index_start = 0
+    index_fim = 0
+    pointer = 0
+    count = 0
+    for slot in index:
+        if slot == 0:
+            count += 1
+        else:
+            count = 0
+            index_start = pointer +1
+        if count == cost:
+            index_fim = pointer
+        pointer += 1
+    
+
 
 #----------------Main-------------
-path = "teste.txt"
+path = "equations.txt"
+inicialize_memory()
 lista_processo = openfile(path)
+id = 0
+"""
 for proc in lista_processo:
-    print(proc)
-
+    #print(proc)
+    size = calculateCost(proc)
+    incert_into_memory(id, cost)
+    id += 1
+"""
+incert_into_memory(0,5)
 
